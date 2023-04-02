9e63225608b9eb05a902f0bddaf498dad45bea45
MertSaygili <mert71719601@gmail.com>
2023-04-02 07:15:58 +0300

    linear and constraint layout examples added

--- app/src/main/java/com/example/personaldocumentationofkotlin/MainActivity.kt
+++ app/src/main/java/com/example/personaldocumentationofkotlin/MainActivity.kt
@@ -1,12 +1,20 @@
-package com.example.personaldocumentationofkotlin
+package com.example.kotlindocumentation
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // git init
+
+
     }
+
 }
