71666ff073ce252fb3d6bf70e304d0e11c66770e
MertSaygili <mert71719601@gmail.com>
2023-04-03 06:31:49 +0300

    basic_appbar addded

--- app/src/main/java/com/example/kotlindocumentation/MainActivity.kt
+++ app/src/main/java/com/example/kotlindocumentation/MainActivity.kt
@@ -2,16 +2,9 @@ package com.example.kotlindocumentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.custom_toast.*
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,20 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnPickImage.setOnClickListener {
-            Intent(Intent.ACTION_GET_CONTENT).also {
-                it.type = "image/*"
-                startActivityForResult(it, 0)
-            }
-        }
-
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK && requestCode == 0){
-            val uri = data?.data
-            ivPhoto.setImageURI(uri)
-        }
-    }
+
 }
+g
