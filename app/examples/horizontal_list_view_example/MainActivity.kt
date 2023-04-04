38727914f182ea8934a3f42064039303e0fbd1ee
MertSaygili <mert71719601@gmail.com>
2023-04-04 05:48:52 +0300

    viewpager example added, horizontal

--- app/examples/horizontal_list_view_example/MainActivity.kt
+++ app/examples/horizontal_list_view_example/MainActivity.kt
@@ -11,6 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val images = listOf(R.drawable.image, R.drawable.image2, R.drawable.image3)
+        val viewPagerAdapter = ViewPagerAdapter(images)
+
+        viewPager.adapter = viewPagerAdapter
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        viewPager.beginFakeDrag()
+        viewPager.fakeDragBy(-10f)
+        viewPager.endFakeDrag()
 
     }
 }
